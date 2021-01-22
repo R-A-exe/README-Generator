972451ed156e956409f17895c9306262ce6e5898
R-A-exe <royatalla.ra@gmail.com>
2021-01-21 23:30:35 -0500

    commented code

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 
-var licenses = null;
+var licenses = null; //will contain the object licenses stored in licenses.json
 
-async function collectTitles() {
+async function collectTitles() {  //collect the list of titles that the user would like to have in their readme
     const result = await inquirer.prompt([
         {
             name: 'titles',
@@ -18,16 +18,16 @@ async function collectTitles() {
 }
 
 
-async function collectValues(titles) {
+async function collectValues(titles) {  //Collect content of each chosen title
 
-    var fileContent = new Map();
+    var fileContent = new Map(); //store content into this map
 
-    const questions = ['projectName', ...titles];
+    const questions = ['projectName', ...titles];  //add project name to the list of titles
 
-    for (question of questions) {
+    for (question of questions) {  //Loop through titles and ask questions based on title
         switch (question) {
 
-            case 'projectName':
+            case 'projectName': //collect project name
                 var question = await inquirer.prompt([
                     {
                         name: 'value',
@@ -38,7 +38,7 @@ async function collectValues(titles) {
                 fileContent.set('name', question.value);
                 break;
 
-            case 'Description':
+            case 'Description': //collect project description if applicable
                 var question = await inquirer.prompt([
                     {
                         name: 'value',
@@ -49,7 +49,7 @@ async function collectValues(titles) {
                 fileContent.set('Description', question.value);
                 break;
 
-            case 'Installation':
+            case 'Installation': //collect project installation if applicable
                 var question = await inquirer.prompt([
                     {
                         name: 'value',
@@ -60,7 +60,7 @@ async function collectValues(titles) {
                 fileContent.set('Installation', question.value);
                 break;
 
-            case 'Usage':
+            case 'Usage': //collect project usage if applicable
                 var question = await inquirer.prompt([
                     {
                         name: 'value',
@@ -71,9 +71,9 @@ async function collectValues(titles) {
                 fileContent.set('Usage', question.value);
                 break;
 
-            case 'License':
-                var successful = await loadLicenses();
-                if (!successful) {
+            case 'License': //collect project license if applicable
+                var successful = await loadLicenses(); //load licenses object
+                if (!successful) { //stop if failed load
                     return;
                 }
                 var question = await inquirer.prompt([
@@ -86,10 +86,10 @@ async function collectValues(titles) {
                 ]);
                 var license = licenses.licenses[licenses.list.indexOf(question.value)];
                 fileContent.set('License', `This project is covered under the following license: ${question.value}. For more information, please visit [${license.url}](${license.url})`);
-                fileContent.set('badge', license.markdown);
+                fileContent.set('badge', license.markdown); //get badge to be added to read me
                 break;
 
-            case 'Contributing':
+            case 'Contributing': //collect project contribution if applicable
                 var question = await inquirer.prompt([
                     {
                         name: 'value',
@@ -100,7 +100,7 @@ async function collectValues(titles) {
                 fileContent.set('Contributing', question.value);
                 break;
 
-            case 'Tests':
+            case 'Tests': v//collect project testing if applicable
                 var question = await inquirer.prompt([
                     {
                         name: 'value',
@@ -111,7 +111,7 @@ async function collectValues(titles) {
                 fileContent.set('Tests', question.value);
                 break;
 
-            case 'Questions':
+            case 'Questions': //collect email, github account and ways to reach author for questions if applicable
                 var question = await inquirer.prompt([
                     {
                         name: 'email',
@@ -134,7 +134,7 @@ async function collectValues(titles) {
                 fileContent.set('Questions', `Email: ${question.email}\n\nGithub: ${question.github}\n\n${question.questions}`);
                 break;
 
-            case 'Custom section':
+            case 'Custom section': //collect title and content of custom section if applicable
                 while (true) {
                     var question = await inquirer.prompt([
                         {
@@ -149,7 +149,7 @@ async function collectValues(titles) {
                         },
 
                     ]);
-                    fileContent.set(question.title, question.content);
+                    fileContent.set(question.title, question.content); //ask user if they would like to add another custom section
                     var moreCustom = await inquirer.prompt([
                         {
                             name: 'another',
@@ -157,59 +157,58 @@ async function collectValues(titles) {
                             message: 'Would you like to add another custom field?'
                         }
                     ]);
-                    if (!moreCustom.another) {
+                    if (!moreCustom.another) { //I no, exit while loop, else, repeat loop
                         break;
                     }
                 }
                 break;
         }
     }
-    return fileContent;
+    return fileContent; //return map
 }
 
 function createContent(fileContent) {
 
-    var text = `# ${fileContent.get('name')}`;
+    var text = `# ${fileContent.get('name')}`; //load project name to text
+    fileContent.delete('name');                 //remove project name from map
 
-    fileContent.delete('name');
-
-    if (fileContent.has('badge')) {
-        text += `   ${fileContent.get('badge')}`;
-        fileContent.delete('badge');
+    if (fileContent.has('badge')) { //if license was selected
+        text += `   ${fileContent.get('badge')}`; //load badge to text
+        fileContent.delete('badge'); //remove badge from map
     }
     
     text += '\n\n';
 
-    if (fileContent.has('Description')) {
-        text += `## Description\n\n${fileContent.get('Description')}\n\n\n\n`;
-        fileContent.delete('Description');
+    if (fileContent.has('Description')) { //if description was selected
+        text += `## Description\n\n${fileContent.get('Description')}\n\n\n\n`; //load description into text
+        fileContent.delete('Description'); //remove dsecription from map
     }
 
-    var table = "## Table of content\n\n";
+    var table = "## Table of content\n\n"; //load table of content title
     var rest = "";
 
-    for (let [key, value] of fileContent) {
+    for (let [key, value] of fileContent) {  //for each remaining item in the map
 
-        table += `### [${key}](##-${key.split(" ").join("-")})\n\n`;
-        rest += `## ${key}\n\n${value}\n\n\n\n`;
+        table += `### [${key}](##-${key.split(" ").join("-")})\n\n`;  //add its title to the table of content
+        rest += `## ${key}\n\n${value}\n\n\n\n`;        //add its title and content to the rest variable that will be concatinated to the text variable
     }
 
-    text += table + '\n\n' + rest;
+    text += table + '\n\n' + rest; //add the table to the text variable (which contains title, badge and description), then add the rest of the content
 
-    return text;
+    return text; //return the text
 }
 
 
-function printContent(text) {
+function printContent(text) { //write content to file using fs.
     fs.writeFile('README.md', text, err => err ? console.log("Something went wrong, please ensure that all dependencies are installed.") : null);
     console.log("All done! Please retrieve your README file from this directory.")
 }
 
-async function loadLicenses() {
+async function loadLicenses() { //function to load licenses from the json file.
 
     const content = util.promisify(fs.readFile);
     try {
-        licenses = JSON.parse(await content('assets/licenses.json', 'utf8'));
+        licenses = JSON.parse(await content('assets/licenses.json', 'utf8')); //load object into global variable
         return true;
 
     } catch (e) {
@@ -223,10 +222,10 @@ async function loadLicenses() {
 }
 
 (async function () {
-    const titles = await collectTitles();
-    const fileContent = await collectValues(titles);
-    const text = createContent(fileContent);
-    printContent(text);
+    const titles = await collectTitles(); //await titles selection, then
+    const fileContent = await collectValues(titles); //await content of each title, then
+    const text = createContent(fileContent);// create the text from content, then
+    printContent(text);//print the text to file.
 })();
 
 
